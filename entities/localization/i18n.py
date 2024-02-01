7b40dc272cfce81a791e776548cfdba499b0d0d9
OrnarasUS <85742833+ornarasus@users.noreply.github.com>
2024-02-01 05:58:20 +0000

    Added comments

--- entities/localization/i18n.py
+++ entities/localization/i18n.py
@@ -9,10 +9,11 @@ class I18n:
 
     def __init__(self):
         self.__langs = {}
-        for f in os.listdir("locale/"):
-            if re.match(r"^[a-z]+_[A-Z]+\.json$", f) is not None:
-                with open(f"locale/{f}", "r", encoding="UTF-8") as fp:
-                    self.__langs[f.split(".")[0]] = Language(json.load(fp))
+        for f in os.listdir("locale/"):                                         # Search file in directory %ProjectDir%/locale/
+            if re.match(r"^[a-z]+_[A-Z]+\.json$", f) is not None:               # Filter of files: xx_XX.json
+                with open(f"locale/{f}", "r", encoding="UTF-8") as fp:          # Open file (read-only)
+                    self.__langs[f.split(".")[0]] = Language(json.load(fp))     # Add object of Language class with parsed data
 
     def __get__(self, _, __):
         return self.__langs[os.environ["LANGUAGE"]]
+    
